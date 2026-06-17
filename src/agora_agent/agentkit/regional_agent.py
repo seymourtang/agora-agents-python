003660cee9bfda6368267559da8db26c8f6efbff
plutoless <qianze.zhang@hotmail.com>
2026-06-17 12:04:06 -0700

    fix(regional_agent): narrow return type only, not vendor params
    Narrowing the param types statically enforced the very vendor/area
    compatibility PR #37 says it does NOT enforce (and which tests exercise
    cross-region). Keep the -> CNAgent/-> GlobalAgent return narrowing for fluent
    chaining; accept the base vendor types. Valid override, no type: ignore.

--- src/agora_agent/agentkit/regional_agent.py
+++ src/agora_agent/agentkit/regional_agent.py
@@ -108,31 +108,31 @@ GlobalAvatar = typing.Union[AkoolAvatar, LiveAvatarAvatar, AnamAvatar, GenericAv
 
 
 class CNAgent(Agent):
-    def with_stt(self, vendor: CNSTT) -> "CNAgent":
-        return typing.cast("CNAgent", super().with_stt(typing.cast(BaseSTT, vendor)))
+    def with_stt(self, vendor: BaseSTT) -> "CNAgent":
+        return typing.cast("CNAgent", super().with_stt(vendor))
 
-    def with_llm(self, vendor: CNLLM) -> "CNAgent":
-        return typing.cast("CNAgent", super().with_llm(typing.cast(BaseLLM, vendor)))
+    def with_llm(self, vendor: BaseLLM) -> "CNAgent":
+        return typing.cast("CNAgent", super().with_llm(vendor))
 
-    def with_tts(self, vendor: CNTTS) -> "CNAgent":
-        return typing.cast("CNAgent", super().with_tts(typing.cast(BaseTTS, vendor)))
+    def with_tts(self, vendor: BaseTTS) -> "CNAgent":
+        return typing.cast("CNAgent", super().with_tts(vendor))
 
-    def with_avatar(self, vendor: CNAvatar) -> "CNAgent":
-        return typing.cast("CNAgent", super().with_avatar(typing.cast(BaseAvatar, vendor)))
+    def with_avatar(self, vendor: BaseAvatar) -> "CNAgent":
+        return typing.cast("CNAgent", super().with_avatar(vendor))
 
 
 class GlobalAgent(Agent):
-    def with_stt(self, vendor: GlobalSTT) -> "GlobalAgent":
-        return typing.cast("GlobalAgent", super().with_stt(typing.cast(BaseSTT, vendor)))
+    def with_stt(self, vendor: BaseSTT) -> "GlobalAgent":
+        return typing.cast("GlobalAgent", super().with_stt(vendor))
 
-    def with_llm(self, vendor: GlobalLLM) -> "GlobalAgent":
-        return typing.cast("GlobalAgent", super().with_llm(typing.cast(BaseLLM, vendor)))
+    def with_llm(self, vendor: BaseLLM) -> "GlobalAgent":
+        return typing.cast("GlobalAgent", super().with_llm(vendor))
 
-    def with_tts(self, vendor: GlobalTTS) -> "GlobalAgent":
-        return typing.cast("GlobalAgent", super().with_tts(typing.cast(BaseTTS, vendor)))
+    def with_tts(self, vendor: BaseTTS) -> "GlobalAgent":
+        return typing.cast("GlobalAgent", super().with_tts(vendor))
 
-    def with_avatar(self, vendor: GlobalAvatar) -> "GlobalAgent":
-        return typing.cast("GlobalAgent", super().with_avatar(typing.cast(BaseAvatar, vendor)))
+    def with_avatar(self, vendor: BaseAvatar) -> "GlobalAgent":
+        return typing.cast("GlobalAgent", super().with_avatar(vendor))
 
 
 RegionalAgent = typing.Union[CNAgent, GlobalAgent]
